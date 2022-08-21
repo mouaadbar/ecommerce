79d9e253ce38fbca74691952871450b6294634f6
unknown <mouaadbarroudi@gmail.com>
2022-08-21 12:58:55 +0200

    first commit

---
+++ src/test/java/com/mouad/ecommouad/EcommouadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mouad.ecommouad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommouadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
